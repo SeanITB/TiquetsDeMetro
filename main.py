276ba835e31786b784515794062897c106e4202c
sean.costelloe.7e5 <sean.costelloe.7e5@itb.cat>
2024-09-27 17:36:14 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+
+class TiketMachine:
+
+    def __init__(self):
+        self.stopCode = "4321"
+        self.namesTypeTickets = tickets = {
+            1 : "Bitllet senzill",
+            2 : "TCasual"
+
+        }
+
+
+    #Tipus de bitllet
+    def ticketType(self, nameTypeTicket):
+        if
+
+    #Numero de Bitllets
+
+    #Preu Total
+
+    #Tiquet
